dc2492b51f34b9bbe0ac244e96e9a7bec86de1ac
jaalmario <jovanniealmario@gmail.com>
2024-03-03 18:58:52 +0800

    Add custom calendar

--- src/views/schedule_settings/ScheduleSettings.jsx
+++ src/views/schedule_settings/ScheduleSettings.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
+import Calendar from 'react-calendar';
+import './calendar.scss';
 
 function ScheduleSettings() {
+  const [date, setDate] = useState(new Date());
+
+  const newdate = useMemo(() => {
+    let max = new Date(date);
+    max.setDate(max.getDate() + 10);
+    return max;
+  }, [date])
+
+  const maxDate = useMemo(() => {
+    let max = new Date(newdate);
+    max.setDate(max.getDate() + 25);
+    return max;
+  }, [date])
   return (
     <div>
-      Schedule Settings page!
+      <div>
+        <Calendar
+          maxDate={maxDate}
+          minDate={date}
+          onClickDay={() => { console.log(date) }}
+          showWeekNumbers={true}
+          minDetail='year'
+          defaultView='month' />
+      </div>
     </div>
   )
 }
