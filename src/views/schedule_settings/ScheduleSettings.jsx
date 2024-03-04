b3a143ac0aae79f6083fe6c269b2e83278876627
jaalmario <jovanniealmario@gmail.com>
2024-03-04 18:06:20 +0800

    Create GameSchedule Page

--- src/views/schedule_settings/ScheduleSettings.jsx
+++ src/views/schedule_settings/ScheduleSettings.jsx
@@ -1,32 +1,66 @@
-import React, { useState, useMemo } from 'react'
-import Calendar from 'react-calendar';
-import './calendar.scss';
+import React, { useState, useMemo } from 'react';
+import CustomVerticalTab from '../../components/tab/CustomVerticalTab';
+import CustomTab from '../../components/tab/CustomTab';
 
-function ScheduleSettings() {
-  const [date, setDate] = useState(new Date());
+import './scheduleSettings.scss';
+import { companyGames } from '../../helper/mocks';
+import ScheduleCalendar from './ScheduleCalendar';
 
-  const newdate = useMemo(() => {
-    let max = new Date(date);
-    max.setDate(max.getDate() + 10);
-    return max;
-  }, [date])
+const ScheduleSettings = () => {
 
-  const maxDate = useMemo(() => {
-    let max = new Date(newdate);
-    max.setDate(max.getDate() + 25);
-    return max;
-  }, [date])
+  const [drawTypes, setDrawTypes] = useState([]);
+  const [closingDates, setClosingDates] = useState([]);
+
+  const getMenu = (child) => {
+    console.log(child);
+    return child.map((subtype) => { return { label: subtype.subGameName } })
+  };
+
+  const fetchClosingData = (newValue) => {
+    if (newValue == 0) {
+      setClosingDates('01');
+    }
+    else if (newValue == 1)
+      setClosingDates('02');
+    else
+      setClosingDates('03');
+  }
+
+  const fetchDrawTypesData = (newValue) => {
+    if (newValue == 0) {
+      setDrawTypes('01');
+    }
+    else if (newValue == 1)
+      setDrawTypes('02');
+    else
+      setDrawTypes('03');
+  }
+
+
+
+  const tabs =
+    companyGames.map((game) => {
+      return {
+        label: game.gameName,
+        Component:
+          <div className="tab-container">
+            <div className="tab-header">
+              <h1>Game Schedules</h1>
+            </div>
+            <CustomVerticalTab
+              changeEvent={fetchDrawTypesData}
+              tabList={[
+                { label: "Game Types", isHeader: true },
+                ...game.child?.map((subtype) => { return { label: subtype.subGameName, Component: <ScheduleCalendar /> } }) ?? [{ label: game.gameName, Component: <ScheduleCalendar /> }],
+                { label: "Draw Types", isHeader: true },
+              ]} />
+          </div>
+      }
+    });
+
   return (
-    <div>
-      <div>
-        <Calendar
-          maxDate={maxDate}
-          minDate={date}
-          onClickDay={() => { console.log(date) }}
-          showWeekNumbers={true}
-          minDetail='year'
-          defaultView='month' />
-      </div>
+    <div className='container'>
+      <CustomTab tabList={tabs} />
     </div>
   )
 }
