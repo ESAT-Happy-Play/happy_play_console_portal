fe3606bc165c5e251987ae5db2386e5d24f939b5
emmaacha <emmalynacha@gmail.com>
2024-03-11 02:38:50 +0800

    add game results page layout

--- src/helper/colors.js
+++ src/helper/colors.js
@@ -1,11 +1,12 @@
 export const COLORS = {
-    background: "#F4F3FC",
-    violetMain: "#4845d2",
-    green: "#14A44D",
-    yellow: "#FFDA18",
-    skyBlueHighlight: "#17A1FA",
-    redWarn: "#DC4C64",
-    darkGrey: "#403D39",
-    transparentFont: "#403d3980",
-    violetHover: "#ffffff20",
-}
+  background: "#F4F3FC",
+  violetMain: "#4845d2",
+  green: "#14A44D",
+  yellow: "#FFDA18",
+  skyBlueHighlight: "#17A1FA",
+  redWarn: "#DC4C64",
+  darkGrey: "#403D39",
+  transparentFont: "#403d3980",
+  violetHover: "#ffffff20",
+  maroon: "#33050c",
+};
