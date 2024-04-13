52cc522f2abd0f1c7d80dbc280ecac0342ef132c
emmaacha <emmalynacha@gmail.com>
2024-04-13 09:18:20 +0800

    make new user login page mobile responsive

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -1,24 +1,31 @@
 import "./login.scss";
-import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useForm } from 'react-hook-form';
-import { toast } from 'react-toastify';
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
 import { InputAdornment, IconButton } from "@mui/material";
-import LoginRoundedIcon from '@mui/icons-material/LoginRounded';
+import LoginRoundedIcon from "@mui/icons-material/LoginRounded";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
-import { setCredentials } from '../../redux/reducers/auth/AuthReducer';
-import { setMenuState } from '../../redux/reducers/MenuStateReducer';
+import { setCredentials } from "../../redux/reducers/auth/AuthReducer";
+import { setMenuState } from "../../redux/reducers/MenuStateReducer";
 
 import { StoreExt } from "../../utils/helpers";
 
-import { MuiInput, MuiLoadingButton, ContentLoader } from "../../components/mui";
-import { ValidateUsername, ValidatePassword } from "../../utils/validations/ValidateLogin";
+import {
+  MuiInput,
+  MuiLoadingButton,
+  ContentLoader,
+} from "../../components/mui";
+import {
+  ValidateUsername,
+  ValidatePassword,
+} from "../../utils/validations/ValidateLogin";
 import { AuthService, MenuService } from "../../services";
 
 export const Login = () => {
@@ -35,8 +42,8 @@ export const Login = () => {
     defaultValues: {
       username: "",
       password: "",
-      ipAddress: "192.168.1.1"
-    }
+      ipAddress: "192.168.1.1",
+    },
   });
 
   const { register, handleSubmit, formState } = formLogin;
@@ -48,11 +55,15 @@ export const Login = () => {
   const validate_password = ValidatePassword(eye);
 
   // Handle for password show/hide
-  const handleEye = () => { setEye(!eye); }
-  
+  const handleEye = () => {
+    setEye(!eye);
+  };
+
   // Handle for login submit
   const loginHandler = async (data) => {
-    if(!checkTerm) { settermError(true); }
+    if (!checkTerm) {
+      settermError(true);
+    }
 
     if (checkTerm) {
       setPageLoader(true);
@@ -61,38 +72,48 @@ export const Login = () => {
           let tokenObj = StoreExt.getDecodeJWT(authResp.data.token);
           dispatch(setCredentials(authResp.data));
           // if not aget and master agent
-          if((tokenObj.role === "Agent") || (tokenObj.role === "Master Agent")) {
-            toast.error("Sorry, you are not allowed to access Happy Play dashboard application.");
+          if (tokenObj.role === "Agent" || tokenObj.role === "Master Agent") {
+            toast.error(
+              "Sorry, you are not allowed to access Happy Play dashboard application."
+            );
             setPageLoader(false);
           } else {
             // get current user and menu
-            MenuService.getSecrityGroupeMenu(tokenObj.RoleId).then((menuResp) => {
-              if(menuResp) {
-                dispatch(setMenuState(menuResp.data));
-                window.location.reload(false);
+            MenuService.getSecrityGroupeMenu(tokenObj.RoleId).then(
+              (menuResp) => {
+                if (menuResp) {
+                  dispatch(setMenuState(menuResp.data));
+                  window.location.reload(false);
+                }
+                setPageLoader(false);
               }
-              setPageLoader(false);
-            });
+            );
           }
-        } else { setPageLoader(false); }
+        } else {
+          setPageLoader(false);
+        }
       });
     }
-  }
+  };
 
   useEffect(() => {
     // redirect to dashboard if already login
     if (loginObj !== null && menuObj !== null) {
-      window.location.href = '/';
+      window.location.href = "/";
     }
   }, [loginObj, menuObj]);
 
   return (
     <>
       <div className="login">
-        <div className='container'>
+        <div className="container">
           <div className="content">
             <div className="top">
-              <img src={require('../../assets/happy-play-logo.png')} className="logo" title="Esat Logo" />
+              <img
+                src={require("../../assets/happy-play-logo.png")}
+                className="logo"
+                title="Esat Logo"
+              />
             </div>
             <div className="div-newuser">
               <a href="/login/new">New User? Login Here</a>
@@ -100,68 +121,82 @@ export const Login = () => {
             <div className="div-login-title">
               <h3>Login</h3>
             </div>
-            <form onSubmit={ handleSubmit(loginHandler) } noValidate>
+            <form onSubmit={handleSubmit(loginHandler)} noValidate>
               <div className="body">
-                  
-                  <div className="form-input">
-                    <label htmlFor="mobileNumber">Username</label>
-                    <MuiInput
-                      {...validate_username}
-                      register={register}
-                      isError={ !!errors.username }
-                      errorMsg={ errors.username?.message }/>
-                  </div>
-                  <div className="form-input">
-                    <label htmlFor="password">Password</label>
-                    <MuiInput
-                      {...validate_password}
-                      register={register}
-                      isError={ !!errors.password }
-                      errorMsg={ errors.password?.message }
-                      inputProps={{
-                        endAdornment:<InputAdornment position="end">
-                          <IconButton onClick={ handleEye } size="small">
-                            {!eye ? <VisibilityIcon /> : <VisibilityOffIcon /> }
+                <div className="form-input">
+                  <label htmlFor="mobileNumber">Username</label>
+                  <MuiInput
+                    {...validate_username}
+                    register={register}
+                    isError={!!errors.username}
+                    errorMsg={errors.username?.message}
+                  />
+                </div>
+                <div className="form-input">
+                  <label htmlFor="password">Password</label>
+                  <MuiInput
+                    {...validate_password}
+                    register={register}
+                    isError={!!errors.password}
+                    errorMsg={errors.password?.message}
+                    inputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton onClick={handleEye} size="small">
+                            {!eye ? <VisibilityIcon /> : <VisibilityOffIcon />}
                           </IconButton>
                         </InputAdornment>
-                      }}/>
-                  </div>
-                  <div className="form-button">
-                    <a href="/forgot/password">Forgot Password?</a>
-                    <MuiLoadingButton text="Login" variant="contained" type="submit" 
-                      loading={ pageLoader } size="medium" color="primary"
-                      loadingPosition='end'
-                      icon={ <LoginRoundedIcon/> } />
-                  </div>
-                  
+                      ),
+                    }}
+                  />
+                </div>
+                <div className="form-button">
+                  <a href="/forgot/password">Forgot Password?</a>
+                  <MuiLoadingButton
+                    text="Login"
+                    variant="contained"
+                    type="submit"
+                    loading={pageLoader}
+                    size="medium"
+                    color="primary"
+                    loadingPosition="end"
+                    icon={<LoginRoundedIcon />}
+                  />
+                </div>
               </div>
-              <br/>
+              <br />
               <hr />
               <div className="div-privacy">
                 <div className="checkRequired">
-                  { (termError) ? <span>Required *</span> : '' }
+                  {termError ? <span>Required *</span> : ""}
                 </div>
-                
-                <FormControlLabel className={(!checkTerm) ? 'hasError' : ''}
+
+                <FormControlLabel
+                  className={!checkTerm ? "hasError" : ""}
                   control={
-                    <Checkbox onChange={e => (setCheckTerm(!checkTerm), settermError(!termError))} />
-                  } label={
+                    <Checkbox
+                      onChange={(e) => (
+                        setCheckTerm(!checkTerm), settermError(!termError)
+                      )}
+                    />
+                  }
+                  label={
                     <div className="div-agree">
-                        <div className="div1">
-                          <span>I agree to Happy Play's</span>
-                          <a href={'/privacy'}>Privacy Policy</a>
-                          <span> and </span>
-                        </div>
-                        <div className="div2">
-                          <a href={'/terms'}>Terms of Use</a>
-                        </div>
+                      <div className="agree-text">
+                        <span>
+                          I agree to Happy Play's{" "}
+                          <a href="/privacy">Privacy Policy</a> and{" "}
+                          <a href="/terms">Terms of Use</a>.
+                        </span>
+                      </div>
                     </div>
-                  } />
+                  }
+                />
               </div>
             </form>
           </div>
         </div>
-        <ContentLoader isLoadingPage={ pageLoader } />
+        <ContentLoader isLoadingPage={pageLoader} />
       </div>
     </>
   );
