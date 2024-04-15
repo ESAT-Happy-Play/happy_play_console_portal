ca325cc7efd2147a28cd553fade84ecd8387f6f0
Richard Cabatana <rcjn44308@gmail.com>
2024-04-16 06:44:46 +0800

    dynamic data for custom table

--- src/views/user_verification/UserVerification.jsx
+++ src/views/user_verification/UserVerification.jsx
@@ -12,38 +12,71 @@ const UserVerification = () => {
   const [verificationData, setverificationData] = useState(null);
   const [header, setheader] = useState(1);
 
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [pageSize, setpageSize] = useState(10);
+
   const tabComponents = () => { return [
     { label: "Verification", itemId: 0, isHeader: true },
     { label: "Verification List", itemId: 1, Component: (verificationData !== null) 
-      ? <VerificationListTable data={verificationData} /> 
+      ? <VerificationListTable 
+          page={page} rowsPerPage={rowsPerPage} pageSize={pageSize} 
+          triggerCallback={triggerCallback} data={verificationData} /> 
       : <div style={{padding:'100px'}}>Loading... Please wait.</div> },
     { label: "History", itemId: 2, Component: <div style={{padding:'100px'}}>Not yet!</div> }
   ]};
 
-  const handeTabChange = (newVal) => { setheader(newVal); }
+  const triggerCallback = async (val = 0, reqType = 0) => {
+    setPageLoader(true);
+    let oldData = verificationData;
+    let result = (reqType === 0) ? await handleInitData(val + 1) : await handleInitData(1, val);
 
-  const handleInitData = () => {
-      setPageLoader(true);
-      UserService.getForVerificationData({
-          companyId: null,
-          dateFrom: null,
-          dateTo: null,
-          pagedQuery: {
-              search: "",
-              pageNumber: 0,
-              pageSize: 10,
-              sortOrder: true
-          }
-      }).then((res) => {
-          if (res.success) { 
-            setverificationData(res.data.verificationUsers); 
-          }
-          setPageLoader(false);
+    if (reqType === 0) {
+      result.verificationUsers.map((item) => {
+        oldData.push(item);
       })
+    }
+
+    setverificationData((reqType === 0) ? oldData : result.verificationUsers);
+    setpageSize(parseInt(result.total));
+    setRowsPerPage(parseInt(result.pageSize));
+    setPage(parseInt(result.pageNumber) - 1);
+
+    setPageLoader(false);
   }
 
-  useEffect(() => {
-    handleInitData();
+  const handeTabChange = (newVal) => { setheader(newVal); }
+
+  const handleInitData = (pageNum = null, perPage = null, searchVal = null) => {
+      return new Promise((resolve, reject) => {
+        UserService.getForVerificationData({
+            companyId: null,
+            dateFrom: null,
+            dateTo: null,
+            pagedQuery: {
+                search: (searchVal !== null) ? searchVal : "",
+                pageNumber: (pageNum !== null) ? pageNum : page,
+                pageSize: (perPage !== null) ? perPage : rowsPerPage,
+                sortOrder: true
+            }
+        }).then((res) => {
+            if (res.success) { 
+              return resolve(res.data);
+            } else { reject("Error"); }
+        })
+      });
+  }
+
+  useEffect( () => {
+    setPageLoader(true);
+    handleInitData().then((res) => {
+      setverificationData(res.verificationUsers);
+      setpageSize(parseInt(res.total));
+      setRowsPerPage((res.pageSize));
+      setPage(parseInt(res.pageNumber));
+
+      setPageLoader(false);
+    });
   }, []);
 
   return (
